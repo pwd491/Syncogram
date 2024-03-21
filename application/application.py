274326d1fc6e9bbbf7523cdb6fee8db55c1addee
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-22 01:02:51 +0300

    [code]: fixed multi-getting screensizes

--- application/application.py
+++ application/application.py
@@ -1,18 +1,22 @@
-from ctypes import windll
+from screeninfo import get_monitors
 import flet as ft
 
+SCREENWIDTH = 1920
+SCREENHEIGHT = 1080
 
-
-screensize = windll.user32.GetSystemMetrics(0), \
-    windll.user32.GetSystemMetrics(1)
+for monitor in get_monitors():
+    if monitor.is_primary:
+        SCREENWIDTH: int = monitor.width
+        SCREENHEIGHT: int = monitor.height
+        break
 
 
 def application(page: ft.Page):
     page.title = "Telegram Migrator"
-    page.window_width = screensize[0] * 0.5
-    page.window_height = screensize[1] * 0.7
-    page.window_top = screensize[1] / 6
-    page.window_left = (screensize[0] * 0.5) / 2
+    page.window_width = SCREENWIDTH * 0.5
+    page.window_height = SCREENHEIGHT * 0.7
+    page.window_top = SCREENHEIGHT / 8
+    page.window_left = (SCREENWIDTH * 0.5) / 2
     page.update()
 
 ft.app(target=application)
--- application/test.py
+++ application/test.py
@@ -1,1 +1,10 @@
-from flet.canvas import 
+from screeninfo import get_monitors
+
+SCREENWIDTH = 1280
+SCREENHEIGHT = 960
+
+for monitor in get_monitors():
+    if monitor.is_primary:
+        SCREENWIDTH: int = monitor.width
+        SCREENHEIGHT: int = monitor.height
+        break
