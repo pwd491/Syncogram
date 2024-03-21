1dd3aa451c37dd913b4fe9369d05386bd1ef3408
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-21 04:16:51 +0300

    Initial commit

--- migrator/main.py
+++ migrator/main.py
@@ -1,8 +1,30 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "Telegram Migrator"
+    page.window_width = 800
+    
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-ft.app(main)
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
