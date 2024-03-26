f765261cd095b701260a36d87418bc3cfc42eb00
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-26 17:27:39 +0300

    app: utils colors

--- application/sourcefiles/utils.py
+++ application/sourcefiles/utils.py
@@ -1,5 +1,6 @@
 from typing import Literal
 from screeninfo import get_monitors
+from flet import colors
 
 def screensize() -> tuple[int, int] | tuple[Literal[1920], Literal[1080]]:
     """
@@ -10,3 +11,13 @@ def screensize() -> tuple[int, int] | tuple[Literal[1920], Literal[1080]]:
         if monitor.is_primary:
             return (monitor.width, monitor.height)
     return (1920, 1080)
+
+def clr_on_secondary_container(platform_brightness, theme_mode,):
+    if platform_brightness == "dark" and theme_mode == "system":
+        return colors.with_opacity(0.2, colors.ON_SECONDARY_CONTAINER,)
+    return colors.with_opacity(0.5, colors.ON_SECONDARY_CONTAINER,)
+
+def clr_secondary_container(platform_brightness, theme_mode,):
+    if platform_brightness == "dark" and theme_mode == "system":
+        return colors.with_opacity(0.1, colors.SECONDARY_CONTAINER,)
+    return colors.with_opacity(1, colors.SECONDARY_CONTAINER,)
