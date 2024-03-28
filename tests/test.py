464ef151efa67a651fb9113d41cba93b50af26bc
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-28 04:22:24 +0300

    app: use super().__init__() to build blocks

--- tests/test.py
+++ tests/test.py
@@ -1,58 +1,27 @@
 import flet as ft
 
-class Section1(ft.UserControl):
-    def __init__(self, page: ft.Page):
-        self.section = ft.Container()
-        self.section.width = 250
-        self.section.expand = True
-        self.section.content = ft.Column()
-        self.section.content.expand = True
-        self.section.bgcolor = 'red'
-        super().__init__()
+class UserBar(ft.Column):
+    def __init__(self):
+        super().__init__(
+            controls = [ft.Container(width=250,expand=True,bgcolor='red')],
+        )
 
-    def build(self):
-        return self.section
+class MainContainer(ft.Column):
+    def __init__(self):
+        super().__init__(
+            controls = [ft.Container(expand=True, bgcolor='red')],
+            expand=True,
+        )
 
-class Section2(ft.UserControl):
-    def __init__(self, page: ft.Page):
-        self.section = ft.Container()
-        self.section.expand = True
-        self.section.content = ft.Column()
-        self.section.content.expand = True
-        self.section.bgcolor = 'red'
-        super().__init__()
+class UIManager(ft.Row):
+    def __init__(self):
+        super().__init__(
+            controls = [UserBar(), MainContainer()],
+            expand=True,
+        )
 
-    def build(self):
-        return self.section
-    
-
-class Manager:
-    def __init__(self, x,y):
-        self.sec1 = x
-        self.sec2 = y
-
-    def build(self):
-        return ft.Row([
-            self.sec1,
-            self.sec2
-        ], expand=True)
-
-
-def application(page: ft.Page) -> None:
-    # page.add(
-    #     ft.Row([
-    #         Section1(page),
-    #         Section2(page),
-    #         ft.Container(ft.Column(expand=True), expand=True, bgcolor='red')
-    #     ], expand=True)
-    # )
-    manager = Manager(Section1, Section2)
-    
-
-    # page.add(manager.build())
-
+def main(page: ft.Page):
+    page.add(UIManager())
     page.update()
 
-
-if __name__ == "__main__":
-    ft.app(target=application)
+ft.app(target=main)
