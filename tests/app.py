813f1296ed705ee52770c032c056b097a2bbc160
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-29 13:51:31 +0300

    more OOP

--- tests/app.py
+++ tests/app.py
@@ -1,4 +1,5 @@
 from functools import partial
+import time
 from typing import Any
 from sql import SQLite
 import flet as ft
@@ -54,41 +55,7 @@ class UserBar(ft.Container):
     def __init__(self, page: ft.Page) -> None:
         self.database = SQLite()
         self.page: ft.Page = page
-
-
-        # Labels
-        ### Нужно переделать !!!
-        self.wrapper_from_label = ft.Text()
-        self.wrapper_from_label.value = "From:"
-        self.wrapper_from_label.size = 11
-        self.wrapper_from_label.opacity = 0.5
-
-        self.wrapper_where_label = ft.Text()
-        self.wrapper_where_label.value = "Where:"
-        self.wrapper_where_label.size = 11
-        self.wrapper_where_label.opacity = 0.5
-
-
-        # Buttons
-        self.add_primary_button = ft.OutlinedButton()
-        self.add_primary_button.text = "Add account"
-        self.add_primary_button.icon = ft.icons.ADD
-        self.add_primary_button.expand = True
-        self.add_primary_button.key = "primary"
-        self.add_primary_button.on_click = partial(
-            self.ui_add_account_process,
-            status=self.add_primary_button.key
-        )
-
-        self.add_secondary_button = ft.OutlinedButton()
-        self.add_secondary_button.text = "Add account"
-        self.add_secondary_button.icon = ft.icons.ADD
-        self.add_secondary_button.expand = True
-        self.add_secondary_button.key = "secondary"
-        self.add_secondary_button.on_click = partial(
-            self.ui_add_account_process,
-            status=self.add_secondary_button.key
-        )
+        
 
         self.settings_btn = ft.ElevatedButton()
         self.settings_btn.text = "Settings"
@@ -104,40 +71,11 @@ class UserBar(ft.Container):
         # CustomAlertDialog
         self.window_authentication = AuthenticationDialogProcedure(
             self.page,
-            self.ui_generate_accounts,
+            self,
         )
 
-        # Divider
-        self.divider = ft.Container()
-        self.divider.width = 200
-        self.divider.height = 0.5
-        self.divider.bgcolor = ft.colors.ON_SECONDARY_CONTAINER
-
-
         # Containers
-        ### [Major block to display accounts on navbar]
-
-        # [Account container `WHERE`]
-        self.wrapper_account_primary = ft.Column()
-        self.wrapper_account_primary.controls = [
-            ft.Row([self.wrapper_from_label]),
-            ft.Row([self.divider]),
-            ft.Row([self.add_primary_button]),
-        ]
-
-        self.wrapper_account_secondary = ft.Column()
-        self.wrapper_account_secondary.controls = [
-            ft.Row([self.wrapper_where_label]),
-            ft.Row([self.divider]),
-            ft.Row([self.add_secondary_button]),
-        ]
-
-
-        self.wrapper_accounts_side = ft.Container()
-        self.wrapper_accounts_side.content = ft.Column([
-            self.wrapper_account_primary,
-            self.wrapper_account_secondary,
-        ])
+        self.wrapper_accounts_side = UIGenerateAccounts()
 
 
 
@@ -172,20 +110,28 @@ class UserBar(ft.Container):
         return button
 
 
-    def ui_generate_accounts(self):
-        accounts: list[Any] = self.database.get_accounts()
-        self.wrapper_account_primary.clean()
-        self.wrapper_account_secondary.clean()
-        for account in accounts:
-            if bool(account[2]):
-                self.wrapper_account_primary.controls.insert(
-                    -1, self.ui_account_button(account[0], account[1])
-                )
-            else:
-                self.wrapper_account_secondary.controls.insert(
-                    -1, self.ui_account_button(account[0], account[1])
-                )
-        self.update()
+    # def ui_generate_accounts(self):
+    #     accounts: list[Any] = self.database.get_accounts()
+
+    #     while len(self.wrapper_account_primary.controls) > 3:
+    #         self.wrapper_account_primary.controls.pop(-2)
+    #     self.page.update()
+
+    #     while len(self.wrapper_account_secondary.controls) > 3:
+    #         self.wrapper_account_secondary.controls.pop(-2)
+    #     self.page.update()
+    #     time.sleep(2)
+            
+    #     for account in accounts:
+    #         if bool(account[2]):
+    #             self.wrapper_account_primary.controls.insert(
+    #                 -1, self.ui_account_button(account[0], account[1])
+    #             )
+    #         else:
+    #             self.wrapper_account_secondary.controls.insert(
+    #                 -1, self.ui_account_button(account[0], account[1])
+    #             )
+    #     self.update()
 
 
     def ui_add_account_process(self, e, status):
@@ -195,6 +141,74 @@ class UserBar(ft.Container):
         self.window_authentication.open = True
         self.page.update()
 
+
+class UIGenerateAccounts(ft.UserControl):
+    def __init__(self):
+        self.database = SQLite()
+
+        self.divider = ft.Container()
+        self.divider.width = 200
+        self.divider.height = 0.5
+        self.divider.bgcolor = ft.colors.ON_SECONDARY_CONTAINER
+
+
+        self.account_primary = ft.Column()
+        self.account_primary.controls = [
+            ft.Row([self.label("From:")]),
+            ft.Row([self.divider]),
+            ft.Row([self.add_button("primary")]),
+        ]
+
+        self.account_secondary = ft.Column()
+        self.account_secondary.controls = [
+            ft.Row([self.label("Where:")]),
+            ft.Row([self.divider]),
+            ft.Row([self.add_button("secondary")]),
+        ]
+
+        self.wrapper_column = ft.Column()
+        self.wrapper_column.controls = [
+            self.account_primary,
+            self.account_secondary
+        ]
+
+        self.wrapper = ft.Container()
+        self.wrapper.content = self.wrapper_column
+
+        super().__init__()
+
+    def account_button(self, account_id, account_name) -> ft.ElevatedButton:
+        button = ft.ElevatedButton()
+        button.width = 250
+        button.text = account_name
+        button.icon = ft.icons.ACCOUNT_CIRCLE
+        button.key = account_id
+        button.on_click = ...
+        return button
+
+    def add_button(self, key: str) -> ft.OutlinedButton:
+        button = ft.OutlinedButton()
+        button.text = "Add account"
+        button.icon = ft.icons.ADD
+        button.expand = True
+        button.key = key
+        button.on_click = None
+        return button
+
+    def label(self, text: str) -> ft.Text:
+        label = ft.Text()
+        label.value = text
+        label.size = 11
+        label.opacity = 0.5
+        return label
+    
+    def generate(self):
+        accounts = self.database.get_accounts()
+        ...
+
+
+    def build(self) -> ft.Container:
+        return self.wrapper
 
 
 def application(page: ft.Page) -> None:
