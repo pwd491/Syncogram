1738f524c69ad85885bb81ce8a2a74c85c4d5246
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-03-30 19:17:14 +0300

    sync

--- tests/sql.py
+++ tests/sql.py
@@ -4,6 +4,7 @@ import sqlite3
 class SQLite:
     def __init__(self):
         self.database = sqlite3.connect("test.db", check_same_thread=False)
+        self.users_table = """CREATE TABLE IF NOT EXISTS `users`"""
 
     def get_accounts(self):
         with self.database as connect:
@@ -21,8 +22,17 @@ class SQLite:
     def set_options(self, *args):
         with self.database as connect:
             with closing(connect.cursor()) as cursor:
-                request = cursor.execute("UPDATE INTO `options` WHERE  (name) VALUES (?)", (name,))
+                request = cursor.execute("UPDATE INTO `options` WHERE  (name) VALUES (?)", ())
                 if request:
                     return True
                 return False
     
+    def test(self):
+        with self.database as connect:
+            with closing(connect.cursor()) as cursor:
+                request = cursor.execute("UPDATE `options` SET is_sync_fav = 1123113213 FROM (SELECT user_id FROM account WHERE prime = 1) as account WHERE account.user_id = options.user_id")
+                return request
+            
+if __name__ == "__main__":
+    abs = SQLite()        
+    print(abs.test())
