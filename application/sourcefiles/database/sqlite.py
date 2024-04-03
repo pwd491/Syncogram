21bf80dab0c595822b4174ad2f659c75510108f8
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-04-03 15:57:23 +0300

    app: sql refact

--- application/sourcefiles/database/sqlite.py
+++ application/sourcefiles/database/sqlite.py
@@ -1,6 +1,13 @@
+import sqlite3
 from contextlib import closing
 from typing import Any
-import sqlite3
+
+from consts import (
+    SQL_CREATE_USERS,
+    SQL_CREATE_OPTIONS,
+    SQL_TRIGGER_DROP_OPTIONS,
+    SQL_TRIGGER_DEFAULT_OPTIONS
+)
 
 
 class SQLite:
@@ -8,51 +15,10 @@ class SQLite:
         self.database: sqlite3.Connection = sqlite3.connect(
             "test.db", check_same_thread=False
         )
-        self.users_table = """
-                            CREATE TABLE IF NOT EXISTS users (
-                            user_id INTEGER,
-                            name TEXT NOT NULL,
-                            is_primary BOOLEAN NOT NULL DEFAULT (0),
-                            session TEXT,
-                            PRIMARY KEY (
-                                user_id AUTOINCREMENT
-                                )
-                            )
-                            """
-
-        self.options_table = """
-                            CREATE TABLE IF NOT EXISTS options (
-                                user_id INTEGER REFERENCES users (user_id) UNIQUE,
-                                is_sync_fav INTEGER DEFAULT 0,
-                                is_sync_pin_fav INTEGER DEFAULT 0,
-                                PRIMARY KEY (
-                                    user_id
-                                )
-                            )
-                            """
-
-        self.trigger_insert_options = """
-                            CREATE TRIGGER IF NOT EXISTS defaults_options_on_insert
-                            AFTER INSERT ON users 
-                            BEGIN 
-                                INSERT INTO options 
-                                            VALUES (
-                                                NEW.user_id,
-                                                1,
-                                                0
-                                            );
-                                END;
-                            """
-
-        self.trigger_delete_options = """
-                            CREATE TRIGGER IF NOT EXISTS delete_options_on_delete
-                                    BEFORE DELETE
-                                        ON users
-                            BEGIN
-                                DELETE FROM options
-                                    WHERE (old.user_id = options.user_id);
-                            END;
-                            """
+        self.users_table = SQL_CREATE_USERS
+        self.options_table = SQL_CREATE_OPTIONS
+        self.trigger_insert_options = SQL_TRIGGER_DEFAULT_OPTIONS
+        self.trigger_delete_options = SQL_TRIGGER_DROP_OPTIONS
 
         self.database.cursor().execute(self.users_table).close()
         self.database.cursor().execute(self.options_table).close()
@@ -64,13 +30,13 @@ class SQLite:
             with closing(connect.cursor()) as cursor:
                 return cursor.execute("SELECT * FROM users").fetchall()
 
-    def add_user(self, id: int, name: str, is_primary: int, session: str) -> bool:
+    def add_user(self, user_id: int, name: str, is_primary: int, session: str) -> bool:
         with self.database as connect:
             with closing(connect.cursor()) as cursor:
                 request = cursor.execute(
-                    "INSERT INTO `users` (user_id, name, is_primary, session) VALUES (?,?,?,?)",
+                    "INSERT INTO `users` VALUES (?,?,?,?)",
                     (
-                        id,
+                        user_id,
                         name,
                         is_primary,
                         session,
@@ -92,4 +58,5 @@ class SQLite:
                     "UPDATE `options` SET is_sync_fav = (?), is_sync_pin_fav = (?) FROM (SELECT user_id FROM users WHERE is_primary = 1) as users WHERE (options.user_id = users.user_id)",
                     (*args,),
                 )
-                return True if request else False
+                return bool(request)
+
