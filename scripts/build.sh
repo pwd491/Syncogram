53904c44c98e78fdb8d7c8b7187f23c11e3ae31c
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-04-18 15:47:52 +0300

    APP: script to fast build unix systems

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd Syncogram;
+flet pack application.py \
+    --name "Syncogram" \
+    --icon "assets/logo/icns/duck512x512.icns" \
+    --add-binary locales:locales \
+    --add-data assets:assets \
+    --add-data config.json:. \
+    --distpath ../craft;
+cd ../;
