74ea74cbc7b70757bd8c88bffdbbea5c1dd190de
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-04-19 23:18:00 +0300

    app: fast builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,11 @@ cd Syncogram;
 flet pack application.py \
     --name "Syncogram" \
     --icon "assets/logo/icns/duck512x512.icns" \
+    --product-name "Syncogram Application" \
+    --product-version "0.0.1" \
+    --company-name "Syncogram Application" \
+    --copyright "GPL-3.0" \
+    --file-version "0.0.1" \
     --add-binary locales:locales \
     --add-data assets:assets \
     --add-data config.json:. \
