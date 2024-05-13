53a6ae4fd2d9bd9ce20f7b2040a5595a0db1f515
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-05-13 17:06:11 +0300

    app: fixed locales

--- Syncogram/application.py
+++ Syncogram/application.py
@@ -3,6 +3,8 @@ import os
 import gettext
 import flet as ft
 
+from locale import getlocale
+
 from sourcefiles import UserBar
 from sourcefiles import MainWindow
 from sourcefiles import WelcomeScreenAnimation
@@ -12,11 +14,19 @@ from sourcefiles.utils import newest_version
 
 cfg = config()
 
-script_dir = os.path.dirname(__file__)
-pth = os.path.join(script_dir, "locales")
+path_to_locales = os.path.abspath(os.path.join(os.path.dirname(__file__), "locales"))
 
-translations = gettext.translation('base', pth, fallback=True)
-_ = translations.gettext
+DOMAIN = "base"
+
+ru = gettext.translation(DOMAIN, path_to_locales, ["ru"], fallback=True)
+en = gettext.translation(DOMAIN, path_to_locales, ["en"], fallback=True)
+
+if getlocale()[0] in ["Russian_Russia","ru_RU"]:
+    ru.install()
+    _ = ru.gettext
+else:
+    en.install()
+    _ = en.gettext
 
 async def application(page: ft.Page) -> None:
     """Entry point of application"""
