ce7a00b84727f395faccf4cc71923985e334e646
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-05-13 17:01:51 +0300

    github: fixed builds data string

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,3 @@
 cd Syncogram
-flet pack application.py --name "Syncogram" --icon "assets/logo/icns/duck512x512.icns" --product-name "Syncogram Application" --product-version "0.0.1" --company-name "Syncogram Application" --copyright "GPL-3.0" --file-version "0.0.1" --add-binary locales:locales --add-data assets:assets --add-data config.json:. --distpath ../craft
+flet pack application.py --name "Syncogram" --icon "assets/logo/icns/duck512x512.icns" --product-name "Syncogram Application" --product-version "0.0.1" --company-name "Syncogram Application" --copyright "GPL-3.0" --file-version "0.0.1" --add-data "locales;locales" --add-data "assets;assets" --add-data "config.json;." --distpath ../craft
 cd ../
