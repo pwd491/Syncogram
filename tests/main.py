8d4dbdae03caebf3a0a8119226e8e6b75caec4bb
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-05-31 18:31:02 +0300

    test

--- tests/main.py
+++ tests/main.py
@@ -3,15 +3,11 @@ import datetime
 import random
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from telethon.tl.patched import MessageService
-from telethon.types import Message, TypeInputFile, Photo
-from telethon.tl.functions.messages import SendMessageRequest
-from telethon.tl.functions.photos import (
-    UploadProfilePhotoRequest,
-    UpdateProfilePhotoRequest,
-)
-from telethon.errors import MultiError
-from functools import partial
+from telethon.tl import types
+from telethon.tl.functions import messages
+from telethon import events
+from telethon import errors
+
 from environments import API_HASH, API_ID, R_SESSION, S_SESSION
 
 sender = TelegramClient(StringSession(S_SESSION), API_ID, API_HASH)
@@ -24,27 +20,81 @@ async def main():
         await sender.connect()
         await recepient.connect()
 
-    photos = await sender.get_profile_photos("me")
-    image_extension = ".jpeg"
-    video_extension = ".mp4"
-    photo: Photo
-    for photo in reversed(photos):
-        blob = await sender.download_media(photo, bytes)
-        name = "Syncogram_" + datetime.datetime.strftime(
-            photo.date, "%Y_%m_%d_%H_%M_%S"
-        )
-        if not photo.video_sizes:
-            file = await recepient.upload_file(
-                blob,
-                file_name=name + image_extension
+    s_entity: types.User = await sender.get_entity('me')
+    r_entity: types.User = await recepient.get_entity('me')
+
+    timeout = 5
+
+    @recepient.on(events.NewMessage(
+        from_users=[s_entity.username], func=lambda e: e.message.gif)
+    )
+    async def gifhook(event: events.NewMessage.Event):
+        gif: types.Document = event.message.gif
+        try:
+            await recepient(
+                messages.SaveGifRequest(
+                    types.InputDocument(
+                        gif.id,
+                        gif.access_hash,
+                        gif.file_reference
+                    ),
+                    unsave=False
+                )
             )
-            await recepient(UploadProfilePhotoRequest(file=file))
-            continue
-        file = await recepient.upload_file(
-            blob,
-            file_name=name + video_extension
-        )
-        await recepient(UploadProfilePhotoRequest(video=file))
+        except Exception as e:
+            print(e)
+
+    stickers_list: types.messages.AllStickers = await sender(
+        messages.GetAllStickersRequest(0)
+    )
+    stickers: list[types.TypeStickerSet] = stickers_list.sets
+
+    emojis_list: types.messages.AllStickers = await sender(
+        messages.GetEmojiStickersRequest(0)
+    )
+    emojis: list[types.TypeStickerSet] = emojis_list.sets
+
+    gifs_list: types.messages.SavedGifs = await sender(
+        messages.GetSavedGifsRequest(0)
+    )
+    gifs: list[types.TypeDocument] = gifs_list.gifs
+    
+    will_delete = []
+    if gifs:
+        for i, gif in enumerate(reversed(gifs), 1):
+            await asyncio.sleep(timeout)
+            message = await sender.send_file(r_entity.username, gif)
+            will_delete.append(message)
+
+    if will_delete:
+        await sender.delete_messages(r_entity.username, will_delete)
+
+    if stickers:
+        for i, sticker in enumerate(reversed(stickers), 1):
+            await asyncio.sleep(timeout)
+            await recepient(
+                messages.InstallStickerSetRequest(
+                    stickerset=types.InputStickerSetShortName(
+                        sticker.short_name
+                    ),
+                    archived=False
+                )
+            )
+
+    if emojis:
+        for i, emoji in enumerate(reversed(emojis), 1):
+            await asyncio.sleep(timeout)
+            try:
+                await recepient(
+                    messages.InstallStickerSetRequest(
+                        stickerset=types.InputStickerSetShortName(
+                            emoji.short_name
+                        ),
+                        archived=False
+                    )
+                )
+            except errors.StickersetInvalidError:
+                continue
 
 
 if __name__ == "__main__":
