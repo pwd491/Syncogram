11eea75c811ee05cbf7f1a8360a787f0e7737725
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-06-23 12:38:53 +0300

    project: catch more exceptions

--- Syncogram/sourcefiles/algorithms/sync_account_bots.py
+++ Syncogram/sourcefiles/algorithms/sync_account_bots.py
@@ -13,13 +13,30 @@ logger = logging()
 
 @logger.catch()
 @autoconnect
-async def sync_bots(ui: Task, **kwargs):
+async def sync_bots(ui: Task, **kwargs) -> None:
+    """The algorithm for synchronizing bots in telegram."""
     ui.default()
 
     sender: UserClient = kwargs["sender"]
     recepient: UserClient = kwargs["recepient"]
 
-    dialogs: list[Dialog] = await sender.get_dialogs()
+    try:
+        dialogs: list[Dialog] = await sender.get_dialogs()
+    except (
+        errors.InputConstructorInvalidError,
+        errors.OffsetPeerIdInvalidError,
+        errors.SessionPasswordNeededError,
+        errors.TimeoutError
+    ) as error:
+        logger.critical(error)
+        ui.unsuccess(error)
+        return
+    except errors.FloodWaitError as flood:
+        logger.warning(flood)
+        ui.cooldown(flood)
+        asyncio.sleep(flood.seconds)
+        ui.uncooldown()
+        dialogs: list[Dialog] = await sender.get_dialogs()
 
     bots: list[types.User] = []
     dialog: Dialog
@@ -30,12 +47,23 @@ async def sync_bots(ui: Task, **kwargs):
 
     ui.progress_counters.visible = True
     ui.total = len(bots) - 1
+
+    timeout = 5 if ui.total < 50 else 10
+
     bot: types.User
     for bot in bots:
-        await asyncio.sleep(5)
+        await asyncio.sleep(timeout)
         try:
             await recepient.send_message(bot.username, "/start")
             ui.value += 1
-        except (errors.YouBlockedUserError, errors.UserIsBlockedError) as e:
-            logger.error(e)
+        except (errors.YouBlockedUserError, errors.UserIsBlockedError) as error:
+            logger.error(error)
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.cooldown(flood)
+            timeout += 5
+            asyncio.sleep(flood.seconds)
+            ui.uncooldown()
+            await recepient.send_message(bot.username, "/start")
+            ui.value += 1
     ui.success()
