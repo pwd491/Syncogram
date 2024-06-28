40c0a14ad93b1d3ab9c94756b15abf4a7caeac73
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-06-28 14:11:48 +0300

    project: now synchronizes the notification settings for the dialog

--- Syncogram/sourcefiles/algorithms/sync_account_bots.py
+++ Syncogram/sourcefiles/algorithms/sync_account_bots.py
@@ -2,7 +2,8 @@ import asyncio
 
 from telethon import errors
 from telethon.tl import types
-from telethon.tl.custom.dialog import Dialog
+from telethon.tl import custom
+from telethon.tl.functions import account
 
 from .decorators import autoconnect
 from ..components import Task
@@ -20,50 +21,83 @@ async def sync_bots(ui: Task, **kwargs) -> None:
     sender: UserClient = kwargs["sender"]
     recepient: UserClient = kwargs["recepient"]
 
-    try:
-        dialogs: list[Dialog] = await sender.get_dialogs()
-    except (
-        errors.InputConstructorInvalidError,
-        errors.OffsetPeerIdInvalidError,
-        errors.SessionPasswordNeededError,
-        errors.TimeoutError
-    ) as error:
-        logger.critical(error)
-        ui.unsuccess(error)
-        return
-    except errors.FloodWaitError as flood:
-        logger.warning(flood)
-        ui.cooldown(flood)
-        asyncio.sleep(flood.seconds)
-        ui.uncooldown()
-        dialogs: list[Dialog] = await sender.get_dialogs()
+    while True:
+        try:
+            source: list[custom.dialog.Dialog] = await sender.get_dialogs()
+            break
+        except (
+            errors.InputConstructorInvalidError,
+            errors.OffsetPeerIdInvalidError,
+            errors.SessionPasswordNeededError,
+            errors.TimeoutError
+        ) as error:
+            logger.critical(error)
+            ui.unsuccess("It is not possible to get a list of the sender's bots.")
+            ui.message("It is not possible to get a list of the sender's bots.")
+            return
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.cooldown(flood)
+            asyncio.sleep(flood.seconds)
+            ui.uncooldown()
 
-    bots: list[types.User] = []
-    dialog: Dialog
-    for dialog in dialogs:
+    bots: list[custom.dialog.Dialog] = []
+    for dialog in source:
         if isinstance(dialog.entity, types.User):
             if dialog.entity.bot:
-                bots.append(dialog.entity)
+                bots.append(dialog)
 
     ui.progress_counters.visible = True
     ui.total = len(bots) - 1
 
     timeout = 5 if ui.total < 50 else 10
+    timeout_additional = 2.5
 
-    bot: types.User
+    bot: custom.dialog.Dialog
     for bot in bots:
-        await asyncio.sleep(timeout)
-        try:
-            await recepient.send_message(bot.username, "/start")
-            ui.value += 1
-        except (errors.YouBlockedUserError, errors.UserIsBlockedError) as error:
-            logger.error(error)
-        except errors.FloodWaitError as flood:
-            logger.warning(flood)
-            ui.cooldown(flood)
-            timeout += 5
-            asyncio.sleep(flood.seconds)
-            ui.uncooldown()
-            await recepient.send_message(bot.username, "/start")
-            ui.value += 1
+        entity: types.User = bot.entity
+        dialog: types.Dialog = bot.dialog
+        while True:
+            try:
+                await asyncio.sleep(timeout)
+                await recepient.send_message(entity.username, "/start")
+                break
+            except (errors.YouBlockedUserError, errors.UserIsBlockedError) as error:
+                logger.error(error)
+                ui.message(f"Unable to sync: @{entity.username}", True)
+                break
+            except errors.FloodWaitError as flood:
+                logger.warning(flood)
+                ui.cooldown(flood)
+                timeout += 5
+                asyncio.sleep(flood.seconds)
+                ui.uncooldown()
+        
+        while True:
+            try:
+                await asyncio.sleep(timeout_additional)
+                await recepient(account.UpdateNotifySettingsRequest(
+                    peer=entity.username,
+                    settings=types.InputPeerNotifySettings(
+                        show_previews=dialog.notify_settings.show_previews,
+                        mute_until=dialog.notify_settings.mute_until,
+                        sound=types.NotificationSoundDefault(),
+                        stories_muted=dialog.notify_settings.stories_muted,
+                        stories_hide_sender=dialog.notify_settings.stories_hide_sender,
+                        stories_sound=types.NotificationSoundDefault()
+                    )
+                ))
+                break
+            except errors.PeerIdInvalidError as error:
+                logger.warning(error)
+                ui.message(f"Failed to synchronize notification settings for: @{entity.username}", True)
+                break
+            except errors.FloodWaitError as flood:
+                logger.warning(flood)
+                ui.message(flood)
+                ui.cooldown(flood)
+                timeout_additional += 5
+                await asyncio.sleep(flood.seconds)
+                ui.uncooldown()
+        ui.value += 1
     ui.success()
--- Syncogram/sourcefiles/algorithms/sync_account_public_channels_groups.py
+++ Syncogram/sourcefiles/algorithms/sync_account_public_channels_groups.py
@@ -1,7 +1,8 @@
+import datetime
 import asyncio
 
 from telethon import errors
-from telethon.tl.functions import channels, messages
+from telethon.tl.functions import channels, messages, account
 from telethon.tl import types
 from telethon.tl.custom.dialog import Dialog
 
@@ -34,8 +35,45 @@ async def sync_public_channels_and_groups(ui: Task, **kwargs):
     sender: UserClient = kwargs["sender"]
     recepient: UserClient = kwargs["recepient"]
 
-    source = await sender.get_dialogs()
-    recepient_channels = await recepient.get_dialogs()
+    while True:
+        try:
+            source: list[Dialog] = await sender.get_dialogs()
+            break
+        except (
+            errors.InputConstructorInvalidError,
+            errors.OffsetPeerIdInvalidError,
+            errors.SessionPasswordNeededError,
+            errors.TimeoutError
+        ) as error:
+            logger.critical(error)
+            ui.unsuccess("It is not possible to get a list of the sender's channels.")
+            ui.message("It is not possible to get a list of the sender's channels.")
+            return
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.cooldown(flood)
+            asyncio.sleep(flood.seconds)
+            ui.uncooldown()
+
+    while True:
+        try:
+            recepient_channels: list[Dialog] = await sender.get_dialogs()
+            break
+        except (
+            errors.InputConstructorInvalidError,
+            errors.OffsetPeerIdInvalidError,
+            errors.SessionPasswordNeededError,
+            errors.TimeoutError
+        ) as error:
+            logger.critical(error)
+            ui.unsuccess("It is not possible to get a list of the recepient's channels.")
+            ui.message("It is not possible to get a list of the recepient's channels.")
+            return
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.cooldown(flood)
+            asyncio.sleep(flood.seconds)
+            ui.uncooldown()
 
     channels_list: list[Dialog] = get_public_channels(source)
     recepient_channels: list[Dialog] = get_public_channels(recepient_channels)
@@ -49,12 +87,13 @@ async def sync_public_channels_and_groups(ui: Task, **kwargs):
 
     channel: Dialog
     for channel in channels_list:
-        if channel.entity.id not in r_channels:
+        entity: types.Channel = channel.entity
+        if entity.id not in r_channels:
             while True:
                 try:
                     await asyncio.sleep(timeout)
                     await recepient(
-                        channels.JoinChannelRequest(channel.entity.username)
+                        channels.JoinChannelRequest(entity.username)
                     )
                     break
                 except errors.ChannelsTooMuchError as error:
@@ -67,7 +106,7 @@ async def sync_public_channels_and_groups(ui: Task, **kwargs):
                     errors.ChannelPrivateError
                 ) as error:
                     logger.error(error)
-                    ui.message(f"Unsuccess join to: {channel.entity.title}")
+                    ui.message(f"Unsuccess join to: @{entity.title}", True)
                     break
                 except errors.InviteRequestSentError:
                     break
@@ -79,11 +118,39 @@ async def sync_public_channels_and_groups(ui: Task, **kwargs):
                     await asyncio.sleep(flood.seconds)
                     ui.uncooldown()
 
+            dialog: types.Dialog = channel.dialog
+            while True:
+                try:
+                    await asyncio.sleep(timeout_adc)
+                    await recepient(account.UpdateNotifySettingsRequest(
+                        peer=entity.username,
+                        settings=types.InputPeerNotifySettings(
+                            show_previews=dialog.notify_settings.show_previews,
+                            mute_until=dialog.notify_settings.mute_until,
+                            sound=types.NotificationSoundDefault(),
+                            stories_muted=dialog.notify_settings.stories_muted,
+                            stories_hide_sender=dialog.notify_settings.stories_hide_sender,
+                            stories_sound=types.NotificationSoundDefault()
+                        )
+                    ))
+                    break
+                except errors.PeerIdInvalidError as error:
+                    logger.warning(error)
+                    ui.message(f"Failed to synchronize notification settings for: @{entity.username}", True)
+                    break
+                except errors.FloodWaitError as flood:
+                    logger.warning(flood)
+                    ui.message(flood)
+                    ui.cooldown(flood)
+                    timeout_adc += 5
+                    await asyncio.sleep(flood.seconds)
+                    ui.uncooldown()
+
             if channel.archived:
                 while True:
                     try:
                         await asyncio.sleep(timeout_adc)
-                        await recepient.edit_folder(channel.entity.username, 1)
+                        await recepient.edit_folder(entity.username, 1)
                         break
                     except errors.FloodWaitError as flood:
                         logger.warning(flood)
@@ -98,13 +165,13 @@ async def sync_public_channels_and_groups(ui: Task, **kwargs):
                     try:
                         await asyncio.sleep(timeout_adc)
                         await recepient(messages.ToggleDialogPinRequest(
-                            channel.entity.username,
+                            entity.username,
                             True
                         ))
                         break
                     except errors.FloodWaitError as flood:
                         logger.warning(flood)
-                        ui.message(flood)
+                        ui.message(flood, True)
                         ui.cooldown(flood)
                         timeout_adc += 5
                         await asyncio.sleep(flood.seconds)
