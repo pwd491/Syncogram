5865c421fccd328ddcf05fec68720ec0c7729cf9
Sergey Degtyar <sergeydegtyar@internet.ru>
2024-07-01 13:30:48 +0300

    project: sync users in privacy settings

--- Syncogram/sourcefiles/algorithms/sync_account_privacy.py
+++ Syncogram/sourcefiles/algorithms/sync_account_privacy.py
@@ -15,6 +15,30 @@ logger = logging()
 @autoconnect
 async def sync_privacy_settings(ui: Task, **kwargs):
     """The algorithm for synchronizing privacy settings."""
+    async def users_in_list(rule: types.TypePrivacyRule, users: list[types.User]) -> list[str]:
+        source: list[str] = []
+        if rule.users:
+            for rule_user in rule.users:
+                for glob_user in users:
+                    if rule_user == glob_user.id:
+                        if glob_user.username or glob_user.usernames:
+                            while True:
+                                try:
+                                    x = await recepient.get_input_entity(
+                                        glob_user.username or \
+                                        glob_user.usernames[0].username
+                                    )
+                                    source.append(x)
+                                    break
+                                except errors.FloodWaitError as flood:
+                                    logger.warning(flood)
+                                    ui.message(flood)
+                                    ui.cooldown(flood)
+                                    # timeout += 5
+                                    await asyncio.sleep(flood.seconds)
+                                    ui.uncooldown()
+        return source
+
     ui.default()
 
     sender: UserClient = kwargs["sender"]
@@ -35,7 +59,7 @@ async def sync_privacy_settings(ui: Task, **kwargs):
     ]
 
     ui.progress_counters.visible = True
-    ui.total = len(input_privacies)
+    ui.total = len(input_privacies) + 1
     timeout = 1
 
     for privacy in input_privacies:
@@ -64,21 +88,29 @@ async def sync_privacy_settings(ui: Task, **kwargs):
         for rule in request.rules:
             if isinstance(rule, types.PrivacyValueAllowAll):
                 rules.append(types.InputPrivacyValueAllowAll())
+                continue
 
             if isinstance(rule, types.PrivacyValueAllowUsers):
-                rules.append(types.InputPrivacyValueAllowUsers([]))
+                users = await users_in_list(rule, request.users)
+                rules.append(types.InputPrivacyValueAllowUsers(users))
+                continue
 
             if isinstance(rule, types.PrivacyValueAllowPremium):
                 rules.append(types.InputPrivacyValueAllowPremium())
+                continue
 
             if isinstance(rule, types.PrivacyValueAllowContacts):
                 rules.append(types.InputPrivacyValueAllowContacts())
+                continue
 
             if isinstance(rule, types.PrivacyValueAllowCloseFriends):
                 rules.append(types.InputPrivacyValueAllowCloseFriends())
+                continue
 
             if isinstance(rule, types.PrivacyValueAllowChatParticipants):
-                rules.append(types.InputPrivacyValueAllowChatParticipants([]))
+                users = await users_in_list(rule, request.users)
+                rules.append(types.InputPrivacyValueAllowChatParticipants(users))
+                continue
 
             if isinstance(rule, types.PrivacyValueDisallowAll):
                 r = True
@@ -88,15 +120,21 @@ async def sync_privacy_settings(ui: Task, **kwargs):
                         break
                 if r:
                     rules.append(types.InputPrivacyValueDisallowAll())
+                continue
 
             if isinstance(rule, types.PrivacyValueDisallowUsers):
-                rules.append(types.InputPrivacyValueDisallowUsers([]))
+                users = await users_in_list(rule, request.users)
+                rules.append(types.InputPrivacyValueDisallowUsers(users))
+                continue
 
             if isinstance(rule, types.PrivacyValueDisallowContacts):
                 rules.append(types.InputPrivacyValueDisallowContacts())
+                continue
 
             if isinstance(rule, types.PrivacyValueDisallowChatParticipants):
-                rules.append(types.InputPrivacyValueDisallowChatParticipants([]))
+                users = await users_in_list(rule, request.users)
+                rules.append(types.InputPrivacyValueDisallowChatParticipants(users))
+                continue
 
         while True:
             try:
@@ -122,44 +160,44 @@ async def sync_privacy_settings(ui: Task, **kwargs):
                 await asyncio.sleep(flood.seconds)
                 ui.uncooldown()
 
-        while True:
-            try:
-                data: types.TypeGlobalPrivacySettings = await sender(
-                    account.GetGlobalPrivacySettingsRequest()
-                )
-                break
-            except errors.FloodWaitError as flood:
-                logger.warning(flood)
-                ui.message(flood)
-                ui.cooldown(flood)
-                timeout += 5
-                await asyncio.sleep(flood.seconds)
-                ui.uncooldown()
+    while True:
+        try:
+            data: types.TypeGlobalPrivacySettings = await sender(
+                account.GetGlobalPrivacySettingsRequest()
+            )
+            break
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.message(flood)
+            ui.cooldown(flood)
+            timeout += 5
+            await asyncio.sleep(flood.seconds)
+            ui.uncooldown()
 
-        while True:
-            try:
-                await recepient(
-                    account.SetGlobalPrivacySettingsRequest(
-                        types.TypeGlobalPrivacySettings(
-                            data.archive_and_mute_new_noncontact_peers,
-                            data.keep_archived_unmuted,
-                            data.keep_archived_folders,
-                            data.hide_read_marks,
-                            data.new_noncontact_peers_require_premium
-                        )
+    while True:
+        try:
+            await recepient(
+                account.SetGlobalPrivacySettingsRequest(
+                    types.TypeGlobalPrivacySettings(
+                        data.archive_and_mute_new_noncontact_peers,
+                        data.keep_archived_unmuted,
+                        data.keep_archived_folders,
+                        data.hide_read_marks,
+                        data.new_noncontact_peers_require_premium
                     )
                 )
-                ui.value += 1
-                break
-            except errors.AutoarchiveNotAvailableError as error:
-                logger.error(error)
-                ui.message(error, True)
-                break
-            except errors.FloodWaitError as flood:
-                logger.warning(flood)
-                ui.message(flood)
-                ui.cooldown(flood)
-                timeout += 5
-                await asyncio.sleep(flood.seconds)
-                ui.uncooldown()
+            )
+            ui.value += 1
+            break
+        except errors.AutoarchiveNotAvailableError as error:
+            logger.error(error)
+            ui.message(error, True)
+            break
+        except errors.FloodWaitError as flood:
+            logger.warning(flood)
+            ui.message(flood)
+            ui.cooldown(flood)
+            timeout += 5
+            await asyncio.sleep(flood.seconds)
+            ui.uncooldown()
     ui.success()
